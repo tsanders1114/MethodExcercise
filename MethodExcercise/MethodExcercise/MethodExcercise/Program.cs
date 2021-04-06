4b4cfc44de3016a376b4414f48caf2c2dd6a0d95
Tim Sanders <tsanders1114@gmail.com>
2021-04-05 22:38:54 -0400

    First Commit for excercise

---
+++ MethodExcercise/MethodExcercise/MethodExcercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I want to play a game! Enter your name Now!!!");
+            var userName = Console.ReadLine();
+
+
+            Console.WriteLine("Enter your favorite color now !");
+            var favColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal to eat?");
+            var favAnimal = Console.ReadLine();
+
+            Console.WriteLine("And last question who do you sing like in the shower");
+            var favSinger = Console.ReadLine();
+
+            Console.WriteLine("Yep your pretty weird to me ! ");
+        }
+    }
+}
